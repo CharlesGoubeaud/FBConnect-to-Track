1a1d0836566cafdf12872967637d7a72f5e2732e
Charles Goubeaud <Taylor@charles-goubeauds-macbook-pro-3.local>
2011-07-05 23:34:10 -0400

    config'rd

--- config.php
+++ config.php
@@ -8,21 +8,21 @@
 // add your facebook credentials here
 define('FACEBOOK_APPID', 'xxxxxxxxxxxxxxxxxxx');
 define('FACEBOOK_SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-define('FACEBOOK_FANPAGE_NAME', 'CASH Music');
-define('FACEBOOK_FANPAGE_ID', '120327795611'); // click "edit this page" and see the id in the URL
-define('FACEBOOK_FANPAGE_URL', 'http://www.facebook.com/cashmusic.org'); 
+define('FACEBOOK_FANPAGE_NAME', 'Mutual Co.');
+define('FACEBOOK_FANPAGE_ID', '204795656199305'); // click "edit this page" and see the id in the URL
+define('FACEBOOK_FANPAGE_URL', 'http://www.facebook.com/pages/Mutual-Co/204795656199305'); 
 
 // the messages shown to the visitor
 // no double-quotes please, escape single quotes with \'
 define('WELCOME_MESSAGE', 'Just click the "Connect to Facebook" link. Anyone who likes the page will get a download link.');
-define('ISAFAN_MESSAGE', 'Thanks! Click below for your download:');
-define('NOTAFAN_MESSAGE', 'Facebook reports that you haven\'t liked the page. Please click the like button and try again.');
+define('ISAFAN_MESSAGE', 'Woah you rock! Pass this along to your friends, and click below for your download:');
+define('NOTAFAN_MESSAGE', 'Naughty, Naughty. Facebook reports that you haven\'t liked the page. You should click the like button and try again.');
 
 // facebook share settings - this is what shows up when a user clicks to share
 // no double-quotes please, escape single quotes with \'
-define('FACEBOOK_SHARE_TITLE', 'CASH Music\'s FBConnect To A Track');
-define('FACEBOOK_SHARE_IMAGE', 'http://fbconnect.cashmusic.org/assets/images/fb_share_image.jpg'); // absolute path 100x100
-define('FACEBOOK_SHARE_DESCRIPTION', 'CASH Music is offering free code that enables artists to set up a site for sharing free downloads with Facebook fans. You can download it here.');
+define('FACEBOOK_SHARE_TITLE', Mutual Co. Goodies');
+define('FACEBOOK_SHARE_IMAGE', 'http://a5.sphotos.ak.fbcdn.net/hphotos-ak-snc6/250364_224717674207103_204795656199305_977774_2442188_n.jpg'); // absolute path 100x100
+define('FACEBOOK_SHARE_DESCRIPTION', 'Mutual Co. is offering free content that enables visiters to enjoy unique experiences through media. You can download our goodies here.');
 
 // set secure download to true (for S3 security) or false for straight http download
 define('SECURE_DOWNLOAD', true);
@@ -33,7 +33,7 @@ define('AMAZONS3_BUCKET', 'xxxxxxxxx');
 
 // set your download title
 // escape single quotes with \'
-define('DOWNLOAD_TITLE', 'FBConnect to a Track Code');
+define('DOWNLOAD_TITLE', 'Mutual Co. Goodies: El Cantador Edition');
 // URL or amazon URI (if amazon, do not include bucket)
 define('DOWNLOAD_URI', 'http://your.file/goes.here.zip');
 ?>
--- index.php
+++ index.php
@@ -1,39 +1,46 @@
-<?php
-
-/**
- * @file
- * A single location to store configuration.
- */
-
-// add your facebook credentials here
-define('FACEBOOK_APPID', 'xxxxxxxxxxxxxxxxxxx');
-define('FACEBOOK_SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-define('FACEBOOK_FANPAGE_NAME', 'Mutual Co.');
-define('FACEBOOK_FANPAGE_ID', '204795656199305'); // click "edit this page" and see the id in the URL
-define('FACEBOOK_FANPAGE_URL', 'http://www.facebook.com/pages/Mutual-Co/204795656199305'); 
-
-// the messages shown to the visitor
-// no double-quotes please, escape single quotes with \'
-define('WELCOME_MESSAGE', 'Just click the "Connect to Facebook" link. Anyone who likes the page will get a download link.');
-define('ISAFAN_MESSAGE', 'Woah you rock! Pass this along to your friends, and click below for your download:');
-define('NOTAFAN_MESSAGE', 'Naughty, Naughty. Facebook reports that you haven\'t liked the page. You should click the like button and try again.');
-
-// facebook share settings - this is what shows up when a user clicks to share
-// no double-quotes please, escape single quotes with \'
-define('FACEBOOK_SHARE_TITLE', Mutual Co. Goodies');
-define('FACEBOOK_SHARE_IMAGE', 'http://a5.sphotos.ak.fbcdn.net/hphotos-ak-snc6/250364_224717674207103_204795656199305_977774_2442188_n.jpg'); // absolute path 100x100
-define('FACEBOOK_SHARE_DESCRIPTION', 'Mutual Co. is offering free content that enables visiters to enjoy unique experiences through media. You can download our goodies here.');
-
-// set secure download to true (for S3 security) or false for straight http download
-define('SECURE_DOWNLOAD', true);
-// set amazon credentials if you are using a secure download
-define('AMAZONS3_KEY', 'xxxxxxxxxxxxxxxxxxx');
-define('AMAZONS3_SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-define('AMAZONS3_BUCKET', 'xxxxxxxxx');
-
-// set your download title
-// escape single quotes with \'
-define('DOWNLOAD_TITLE', 'Mutual Co. Goodies: El Cantador Edition');
-// URL or amazon URI (if amazon, do not include bucket)
-define('DOWNLOAD_URI', 'http://your.file/goes.here.zip');
+<?php 
+require_once('includes/head.php'); 
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+<head> 
+<title>Mutual Co.</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="title" content="<?php echo FACEBOOK_SHARE_TITLE; ?>" />
+<meta name="description" content="<?php echo FACEBOOK_SHARE_DESCRIPTION; ?>" />
+<link rel="image_src" href="<?php echo FACEBOOK_SHARE_IMAGE; ?>" / >
+<link rel="icon" type="image/png" href="http://a5.sphotos.ak.fbcdn.net/hphotos-ak-snc6/250364_224717674207103_204795656199305_977774_2442188_n.jpg" /> 
+ 
+<script src="http://ajax.googleapis.com/ajax/libs/mootools/1.2.4/mootools-yui-compressed.js" type="text/javascript"></script>
+<script src="http://connect.facebook.net/en_US/all.js"></script>
+
+<?php 
+	if ($on_facebook) { 
+		require_once('includes/script_fb.php'); 
+		echo '<link href="assets/css/fb.css" rel="stylesheet" type="text/css" />'; 
+	} else {
+		require_once('includes/script_main.php'); 
+		echo '<link href="assets/css/main.css" rel="stylesheet" type="text/css" />';
+	} 
+?>
+
+</head> 
+<body> 
+
+	<div id="fb-root"></div>
+	<div id="mainspc"> 
+
+		<h2>Mutual Co. Goodies</h2>
+		<p style="margin-bottom:40px;">
+		This page uses Facebook's API to give a free download to fans of our
+		Facebook page. All you need to do is be a fan of <a href="http://www.facebook.com/pages/Mutual-Co/204795656199305">Mutual Co. on Facebook</a> 
+		for a free download of the Goodies. You can even use the fanbox below to become a fan.
+		</p>
+
+		<?php require_once('includes/render_components.php'); ?>
+
+		<div class="clearall">.</div>
+	</div> 
+ 
+</body> 
+</html>
